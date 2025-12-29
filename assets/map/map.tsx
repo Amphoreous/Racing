899d0a5166dfa7abc9f0da9e60bd12fbce691cfa
Joel Martínez Arjona <jmartinezarjona03@gmail.com>
2025-12-29 22:31:23 +0100

    Update map assets and configuration for new layout

--- assets/map/map.tsx
+++ assets/map/map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="map" tilewidth="1" tileheight="1" tilecount="5500000" columns="2750">
- <image source="Map.png" width="2750" height="2000"/>
+<tileset version="1.10" tiledversion="1.11.2" name="map" tilewidth="1" tileheight="1" spacing="1" margin="1" tilecount="1375000" columns="1375">
+ <image source="map.png" width="2750" height="2000"/>
 </tileset>
